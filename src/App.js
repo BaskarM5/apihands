e44276533484f1ea0ab7b877cdac3a86a55d0294
Baskar M <baskarvhp98@gmail.com>
2024-10-17 20:44:42 +0530

    API HANDS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(1); 
+  const [todos, setTodos] = useState([]);   
+
+  const temp = () => {
+    const url = `https://jsonplaceholder.typicode.com/users/${count}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setTodos(prevTodos => [...prevTodos, data]);
+        setCount(count + 1);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>BAS</h1>
       </header>
+      
+      <div className="div">
+        <button onClick={temp}>CLICK TO CALL API</button>
+      </div>
+      
+      <div className="res">
+        <h1>Fetched DATA:</h1>
+        {todos.length > 0 ? (
+          todos.map((todo) => ( 
+            <div key={todo.id} className="todo-item">
+              <h3>ID: {todo.id}</h3> 
+              <h3>NAME:{todo.username}</h3>
+              <h3>email: {todo.email}</h3>
+              <h3>phone: {todo.phone}</h3>
+              <h3>website: {todo.website}</h3>
+              <br/>
+            </div>
+          ))
+        ) : (
+          <h3>No data fetched yet</h3>
+        )}
+      </div>
+      
+      <div className="time">
+        NO OF TIMES CALLED: {count}
+      </div>
     </div>
   );
 }
